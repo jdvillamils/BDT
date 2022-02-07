a5a27618ad8c28f00e3313ba7c76b7d8a73accc2
Juan David <jdvillamils@unal.edu.co>
2022-02-07 17:46:04 +0100

    download scrip

---
+++ download_root_files.sh
@@ -0,0 +1,22 @@
+mkdir RootFiles
+
+cd RootFiles/
+
+wget https://cernbox.cern.ch/index.php/s/nfIqrTxXP66wIhg/download
+mv download TMVAppValuesData.root
+wget https://cernbox.cern.ch/index.php/s/oQlywPuEUUNQbNI/download
+mv download TMVAppValues.root
+wget https://cernbox.cern.ch/index.php/s/fwmUt3gmSQydMaU/download
+mv download TMVAppData.root
+wget https://cernbox.cern.ch/index.php/s/CneWtBE0qJVUn9j/download
+mv download TMVApp.root
+wget https://cernbox.cern.ch/index.php/s/vkigASfJiaWeYBh/download 
+mv download TMVABDT.root
+wget https://cernbox.cern.ch/index.php/s/oOafeY1CIO1O76e/download
+mv download splitMC.root
+wget https://cernbox.cern.ch/index.php/s/PSFqvvzucxKjrEt/download
+mv download signal.root
+wget https://cernbox.cern.ch/index.php/s/jD13e53TAnR0IN4/download
+mv download data.root
+wget https://cernbox.cern.ch/index.php/s/wStUruVQdBV6tM9/download
+mv download background.root
